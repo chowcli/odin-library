1c0f628da2f234cb4b3b8629d06dfd3c875dc665
chowcli <136085102+chowcli@users.noreply.github.com>
2023-08-24 21:52:12 +0700

    Add interaction with button, clear form when hit reset button Push input value into object

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// Declare variable
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close");
+const resetBtn = document.querySelector(".reset");
+
+const addBig = document.getElementById("add-big");
+const addSmall = document.getElementById("add-small");
+
+// Interact with modal
+addBig.addEventListener("click", () => modal.showModal());
+closeModal.addEventListener("click", () => modal.close());
+
+// Object constructor for library
 const myLibrary = [];
 
 function Book(title, author, pages, language, date, status) {
@@ -9,25 +22,34 @@ function Book(title, author, pages, language, date, status) {
   this.status = status;
 }
 
-function addBookToLibrary(book) {
+Book.prototype.addBookToLibrary = function (book) {
   myLibrary.push(book);
+};
+
+// Create new book object and take value from user input
+function createBookObject() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("authorName").value;
+  const pages = document.getElementById("pageNums").value;
+  const language = document.getElementById("language").value;
+  const date = document.getElementById("publishDate").value;
+  const status = document.getElementById("readStatus").value;
+
+  const book = new Book(title, author, pages, language, date, status);
+
+  book.addBookToLibrary(book);
 }
 
+addSmall.addEventListener("click", createBookObject);
 
+// Clear form fields
+function clearInput() {
+  document.getElementById("title").value = "";
+  document.getElementById("authorName").value = "";
+  document.getElementById("pageNums").value = "";
+  document.getElementById("language").value = "";
+  document.getElementById("publishDate").value = "";
+  document.getElementById("readStatus").value = "";
+}
 
-
-
-
-
-
-
-
-// Show modal
-const modal = document.querySelector(".modal");
-const addBig = document.getElementById("add-big");
-
-addBig.addEventListener("click", () => modal.showModal());
-
-// Close modal
-const closeModal = document.querySelector(".close");
-closeModal.addEventListener("click", () => modal.close());
+resetBtn.addEventListener("click", clearInput);
